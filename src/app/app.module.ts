8437fca3f6eb02a69b7ec4b4581df077e26a86ec
Srivastava, Pankaj <Pankaj.Srivastava@barco.com>
2017-07-22 16:51:16 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { HeaderComponent } from './header/header.component';
+
+
+import 'hammerjs';
+import {MdButtonModule, MdToolbarModule, MdIconModule, MdTabsModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     FlexLayoutModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
